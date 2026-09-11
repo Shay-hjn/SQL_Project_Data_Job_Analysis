77c1977cc4b57e102420f6d04e6c4919e7269f52
Shay-hjn <s328277@studenti.polito.it>
2026-09-11 19:09:59 +0200

    Fix

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,35 @@
+/*
+Question: What skills are required for the top-paying data analyst jobs?
+*/
+
+WITH top_paying_jobs AS (
+SELECT 
+    job_id,
+    job_title,
+    salary_year_avg,
+    name AS company_name
+FROM
+    job_postings_fact jp
+LEFT JOIN company_dim cd ON jp.company_id = cd.company_id
+WHERE 
+    job_location = 'Anywhere'
+    AND
+    job_title_short = 'Data Analyst'
+    AND 
+    salary_year_avg IS NOT NULL 
+ORDER BY salary_year_avg DESC
+LIMIT 10
+
+)
+
+SELECT
+    tpj.*,
+    skills
+FROM 
+    top_paying_jobs tpj, skills_job_dim sjd, skills_dim sd
+WHERE
+    tpj.job_id = sjd.job_id AND sjd.skill_id = sd.skill_id
+ORDER BY 
+    salary_year_avg DESC
+
+
